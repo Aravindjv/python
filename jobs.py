a66090b83b5058e6a18056c1a254da8ef3dfc50a
Aravindjv <122072070+Aravindjv@users.noreply.github.com>
2023-07-07 09:14:28 +0000

    commit

--- jobs.py
+++ jobs.py
@@ -7,10 +7,10 @@ class job:
 def schedulejobs(jobs,T):
         profit=0
         slot=[-1]*T
-        jobs.sort(key=lambda x:x.profit,reverse=true)
+        jobs.sort(key=lambda x:x.profit,reverse=True)
         for job  in jobs:
-            for j in reversed(range(job.deadline)):
-                if j<T and  slot[j]==-1:
+            for j in reversed(range(min(T,job.deadline))):
+                if  slot[j]==-1:
                     slot[j]=job.taskid
                     profit+=job.profit
         print("the scheduled jobs are:",list(filter(lambda x:x!=-1,slot)))
@@ -19,9 +19,9 @@ if __name__=='__main__':
     jobs=[]
     n=int(input("enter number of jobs"))
     for i in range(n):
-           taskid=int(input("enter taskid for job{}:".format(i+1)))
-           deadline=int(input("enter the deadline for job{}:".format(i+1)))
-           profit=int(input("enter the profit for job{}:".format(i+1)))
-           jobs.append(job(taskid,deadline,profit))
+        taskid=int(input("enter taskid for job{}:".format(i+1)))
+        deadline=int(input("enter the deadline for job{}:".format(i+1)))
+        profit=int(input("enter the profit for job{}:".format(i+1)))
+        jobs.append(job(taskid,deadline,profit))
     T=int(input("enter the deadline limit"))
     schedulejobs(jobs,T)

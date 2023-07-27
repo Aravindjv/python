dd710fcc3e6a061478764c743a4d848753c474f6
Aravindjv <122072070+Aravindjv@users.noreply.github.com>
2023-07-27 06:18:06 +0000

    commit

--- kanpsack.py
+++ kanpsack.py
@@ -14,16 +14,19 @@ def knapsack_max_profit(weights,costs,capacity):
   for i in range(num_items,0,-1):
     if table[i][total_weight]!=table[i-1][total_weight]:
       selected_items.append(i-1)
-      total_weight==weights[i-1]
+      total_weight-=weights[i-1]
+     
   return table[num_items][capacity],selected_items
-weights=[2,3,4,5]
-costs=[10,20,30,40]
-capacity=10
-#weights = input("Enter the weights of the items: ").split()
-#weights = [int(w) for w in weights]
-#costs = input("Enter the costs of the items: ").split()
-#costs = [int(c) for c in costs]
-#capacity = int(input("Enter the capacity of the knapsack: "))
+#weights=[2,3,4,5]
+#costs=[10,20,30,40]
+#capacity=10
+weights=[]
+costs=[]
+weights = input("Enter the weights of the items: ").split()
+weights = [int(w) for w in weights]
+costs = input("Enter the costs of the items: ").split()
+costs = [int(c) for c in costs]
+capacity = int(input("Enter the capacity of the knapsack: "))
 max_profit,selected_items=knapsack_max_profit(weights,costs,capacity)
 print("maximun profit: ",max_profit)
 print("selected coffee beans (index): ",selected_items)
